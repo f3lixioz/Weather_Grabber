dfc5e2fd1b9a017a961ae319ca14835da4137fea
justinthelaw <justinwingchunglaw@gmail.com>
2021-06-21 15:02:08 -0700

    Presentation complete, 1502 21-06-2021

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,4 +1,4 @@
-describe("Home page", () => {
+describe("Home Page", () => {
   beforeEach(() => {
     cy.visit('/')
   })
@@ -15,49 +15,20 @@ describe("Home page", () => {
 
 })
 
-//   it("contains an add recipe button that when clicked opens a form", () => {
-//     cy.findByRole('button').click();
+describe("App Page", () => {
+  beforeEach(() => {
+    cy.visit('/app')
+  })
 
-//     cy.get('form')
-//       .findByRole('button')
-//       .should('exist')
-//   })
+  it("Header contains application name, Weather Grabber", () => {
+    cy.findAllByRole('heading')
+      .should('contain', 'Weather Grabber')
+  })
 
-//   it("contains a form with fields 'Recipe Name' and 'Recipe Instructions' after clicking the 'Add Recipe' button", () => {
-//     cy.findByRole('button').click();
-//     cy.findByRole('textbox', { name: /Recipe name/i }).should('exist')
-//     cy.findByRole('textbox', { name: /instructions/i }).should('exist')
-//   })
+  it("Contains a form for inputting user information", () => {
+    cy.findByPlaceholderText('Name').type("Justin Law")
+    cy.findByPlaceholderText('Email').type("justinwingchunglaw@gmail.com")
+    cy.findByPlaceholderText('Zip Code').type("11714")
+  })
 
-//   it("displays a recipe name and instructions under the 'My Recipes' heading after it has been added through the 'Add Recipe' form", () => {
-//     const recipeName = 'Tofu Scramble Tacos';
-//     cy.findByRole('button').click()
-//     cy.findByRole('textbox', { name: /Recipe name/i }).type(recipeName)
-//     cy.findByRole('textbox', { name: /instructions/i }).type("1. heat a skillet on medium with a dollop of coconut oil {enter} 2. warm flour tortillas")
-
-//     return cy.findByRole('button').click()
-//       .then(() => {
-//         cy.findByRole('listitem', /tofu scramble tacos/i).should('exist')
-//         cy.findByRole('listitem', /1. heat a skillet on medium with a dollop of coconut oil {enter} 2. warm flour tortillas/i).should('exist')
-//       })
-//   })
-
-//   it("displays MULTIPLE (CFU 3) recipe names and instructions under the 'My Recipes' heading after it has been added through the 'Add Recipe' form", () => {
-//     const recipeName = 'Tofu Scramble Tacos';
-//     cy.findByRole('button').click()
-//     cy.findByRole('textbox', { name: /Recipe name/i }).type(recipeName)
-//     cy.findByRole('textbox', { name: /instructions/i }).type("1. heat a skillet on medium with a dollop of coconut oil {enter} 2. warm flour tortillas")
-//     cy.findByRole('button').click()
-
-
-//     const secondRecipeName = 'Water';
-//     cy.findByRole('textbox', { name: /Recipe name/i }).type(secondRecipeName)
-//     cy.findByRole('textbox', { name: /instructions/i }).type("1. take water from tap")
-
-//     return cy.findByRole('button').click()
-//       .then(() => {
-//         cy.findByText('Tofu Scramble Tacos: 1. heat a skillet on medium with a dollop of coconut oil 2. warm flour tortillas').should('exist')
-//         cy.findByText('Water: 1. take water from tap').should('exist')
-//       })
-//   })
-// })
+})
